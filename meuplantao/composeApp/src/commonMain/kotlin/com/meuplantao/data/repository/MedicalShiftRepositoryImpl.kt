ea5fe73ea8d210a92c38692bc722b3b220ad3db8
Roanderson Pinheiro <roandersonfelipepinheiro@gmail.com>
2024-09-01 14:33:37 -0300

    chore: removing files

--- meuplantao/composeApp/src/commonMain/kotlin/com/meuplantao/data/repository/MedicalShiftRepositoryImpl.kt
+++
@@ -1,9 +0,0 @@
-package com.meuplantao.data.repository
-
-import com.meuplantao.domain.MedicalShiftRepository
-
-class MedicalShiftRepositoryImpl() : MedicalShiftRepository {
-    override fun saveMedicalShift() {
-        TODO("Not yet implemented")
-    }
-}
